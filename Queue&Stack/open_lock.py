467226ef1a9b3d3722bff4e2445f6f53fb1c7d79
bwisgood <857464370@qq.com>
2019-08-01 04:36:04 +0800

    bfs

--- Queue&Stack/open_lock.py
+++ Queue&Stack/open_lock.py
@@ -12,92 +12,40 @@ class Solution:
 
     def openLock(self, deadends, target: str) -> int:
         from queue import Queue
-        from copy import deepcopy
-
-        _START = [0, 0, 0, 0]
-        # exclude wrong condition
-        if "".join(list(map(str, list(_START)))) in deadends or len(deadends) > 500:
+        # reduce query time
+        deadends = set(deadends)
+        _START = "0000"
+        if _START in deadends:
             return -1
-
-        # resolve if the deadends is ['0010'] and target is '0019'
-        tl = list(map(int, list(target)))
-        if tl == _START:
-            return 0
-        # 1. create a queue to store next numbers, and initialize the step equal 0
         q = Queue()
         q.put(_START)
+
         step = -1
-
-        # if we need lend other numbers to transform current numbers we can store lend number's index
-        self._LEND = -1
-
-        # 2. while current number exist & target number not found
-        def next_number(self, cur_):
-            unequal_number_index = []
-            # usual
-            for i, e in enumerate(cur_):
-                if e != tl[i]:
-                    unequal_number_index.append(i)
-                    if i == self._LEND:
-                        continue
-                    # to ensure the value in 0 - 9
-                    temp_cur = deepcopy(cur_)
-                    if tl[i] - e > 5 or tl[i] - e < 0:
-                        value = temp_cur[i] - 1
-                        if value < 0:
-                            value = 9
-                        temp_cur[i] = value
-                    else:
-                        temp_cur[i] = (temp_cur[i] + 1) % 10
-
-                    if "".join(list(map(str, temp_cur))) in deadends:
-                        continue
-                    else:
-                        return temp_cur
-
-            # check if _LEND is surplus let the _LEND to -1 and return next_number(cur_)
-            if len(unequal_number_index) == 1:
-                if self._LEND >= 0 and self._LEND in unequal_number_index:
-                    self._LEND = -1
-                    return next_number(self, cur_)
-            # lend a number form others
-            if self._LEND >= 0:
-                return None
-            for i, e in enumerate(cur_):
-                lend_cur = deepcopy(cur_)
-                lend_cur[i] = (e + 1) % 10
-                if "".join(list(map(str, lend_cur))) not in deadends:
-                    self._LEND = i
-                    return lend_cur
-                lend_cur = deepcopy(cur_)
-                temp = e - 1
-                if e - 1 < 0:
-                    temp = 9
-                lend_cur[i] = temp
-                if "".join(list(map(str, lend_cur))) not in deadends:
-                    self._LEND = i
-                    return lend_cur
-
-            return None
-
         while not q.empty():
-            current = q.get()
             step += 1
-            # 2.1 judge if current number is target number
-            if current == tl:
-                return step
-            # 3. add next possible number to the queue
-            next_ = next_number(self, current)
-            # cancel comment next line to check out the step
-            print(next_, end='->')
-            if not next_:
-                continue
-            else:
-                q.put(next_)
+            for size in range(q.qsize()):
+                current = q.get()
+                print(current)
+                if current == target:
+                    return step
+
+                for i in range(4):
+                    if int(target[i]) - int(current[i]) > 5 or int(target[i]) < int(current[i]):
+                        opt = -1
+                    else:
+                        opt = 1
+                    # for add in opt:
+                    next_ = list(current)
+                    next_[i] = str((int(next_[i]) + opt) % 10)
+                    next_ = "".join(next_)
+                    if next_ not in deadends:
+                        q.put(next_)
+                        deadends.add(next_)
         return -1
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.openLock(deadends=["0033","3201","0321","0122","3032","2120","1230","2303","2111","2030"], target="0123"),
+    print(solution.openLock(deadends=["0033", "3201", "0321", "0122", "3032", "2120", "1230", "2303", "2111", "2030"],
+                            target="0123"),
           'step')
