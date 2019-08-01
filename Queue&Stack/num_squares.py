c5906735a4dac5da1bfc12a0b5438792469f0832
bwisgood <bwisgood@163.com>
2019-08-02 00:52:50 +0800

    use bfs

--- Queue&Stack/num_squares.py
+++ Queue&Stack/num_squares.py
@@ -1,18 +1,28 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         from math import sqrt
-        step = 1
+        from queue import Queue
+        seen = set()
+        if sqrt(n).is_integer():
+            return 1
+        step = -1
+        q = Queue()
         max_ = int(sqrt(n))
-        cur = n - max_ ** 2
-        print(max_, "+")
-        while cur != 0:
-            max_ = int(sqrt(max_))
-            print(max_, "+")
-            cur = cur - max_ ** 2
+        q.put(0)
+        while not q.empty():
             step += 1
-        return step
+            for size in range(q.qsize()):
+                cur = q.get()
+                if cur == n:
+                    return step
+
+                for i in range(max_, -1, -1):
+                    temp = cur + (i + 1) * (i + 1)
+                    if temp <= n and temp not in seen:
+                        seen.add(temp)
+                        q.put(temp)
 
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.numSquares(12))
+    print(solution.numSquares(318))
