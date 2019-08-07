cc09645dd0adddc9259a447f3550768ce53973af
bwisgood <857464370@qq.com>
2019-08-08 02:50:23 +0800

    use stack

--- QueueStack/string_decode.py
+++ QueueStack/string_decode.py
@@ -1,62 +1,18 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        # 初始化字符串""
-        # 遇到"["就入栈等待处理
-        # length = len(s)
-        # cur = (0, s[0])
-        if not s:
-            return ""
-        times_stack = []
-        string_stack = []
-
-        """
+        stack, this_str, num = [], '', 0
         for i in s:
-            如果i是数字则说明他后面的字符需要翻倍i次
-            如果是字符则需要加上之前的字符代表即将需要翻倍的字符
-            如果是【 则表示即将开始记录需要翻倍的字符
-            如果是】 则找到上一个【之后的所有字符和上一个需要翻倍的次数去做计算
-            3[ab3[ab3[ab]]]
-            3[ab]2[ab]
-            2
-            ababab ab
-        """
-        base = ""
-        base_num = ""
-        last_c = ""
-        for i in s:
-            if i.isnumeric():
-                # times_stack.append(int(i))
-                base_num += i
-            elif i == "[":
-                # if len(string_stack) != len(times_stack):
-                if base != "":
-                    string_stack.append(base)
-                if base_num:
-                    times_stack.append(int(base_num))
-                base = ""
-                base_num = ""
-                last_c = "["
-            elif i == "]":
-                # 开始计算
-                time = times_stack.pop()
-                # ms = string_stack.pop()
-                if base and last_c == "[":
-                    t = time * base
-                elif base and last_c == "]":
-                    t = time * (string_stack.pop() + base)
-                else:
-                    t = time * string_stack.pop()
-                if string_stack:
-                    bt = string_stack.pop()
-                    string_stack.append(bt + t)
-                else:
-                    string_stack.append(t)
-                base = ""
-                base_num = ""
-                last_c = "]"
-            else:
-                base += i
-        return "".join(string_stack) + base
+            if i.isdigit():
+                num = num * 10 + int(i)
+            elif i.isalpha():
+                this_str += i
+            elif i == '[':
+                stack.append((this_str, num))
+                this_str, num = '', 0
+            else:  # i == ']'
+                last_str, this_num = stack.pop()
+                this_str = last_str + this_num * this_str
+        return this_str
 
 
 if __name__ == '__main__':
